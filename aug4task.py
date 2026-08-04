83191bbecffd4fbd615ed8d3f63b15fb41d974d7
avhadkavita05 <avhadkavita461@gmail.com>
2026-08-04 21:13:10 +0530

    Add files via upload

---
+++ aug4task.py
@@ -0,0 +1,64 @@
+#Task1
+
+print("----------My ID Card---------")
+
+Name = "Kavita Navnath Avhad"
+print(Name)
+
+Age = 20
+print(Age)
+
+City = "Nashik"
+print(City)
+
+DOB = "04-02-2006"
+print(DOB)
+
+College = "GES R.H. Sapat College Of Engineering"
+print(College)
+
+BloodGroup = "AB+"
+print(BloodGroup)
+
+#Task2
+a = 21
+b = 25.5
+c = "Python is simple programming lanluage"
+d = True
+e = 2+3j
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+
+#Task3
+v1 = 100
+v2 = 200
+v3 = 300
+v4 = 100
+print(id(v1))
+print(id(v2))
+print(id(v3))
+print(id(v4))
+
+''' -------OUTPUT------
+PS C:\Users\abc\OneDrive\Desktop\Batch 1341 ws\core python ws> py aug4task.py
+----------My ID Card---------
+Kavita Navnath Avhad
+20
+Nashik
+04-02-2006
+GES R.H. Sapat College Of Engineering
+AB+
+<class 'int'>
+<class 'float'>
+<class 'str'>
+<class 'bool'>
+<class 'complex'>
+140704673747160
+140704673750360
+2147651858896
+140704673747160
+PS C:\Users\abc\OneDrive\Desktop\Batch 1341 ws\core python ws> 
+'''
